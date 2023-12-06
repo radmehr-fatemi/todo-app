2154e7e0373d8094998c859b48efbe86a42318e0
Alireza Fatemi <fa1381138100@gmail.com>
2023-12-06 15:13:24 +0330

    edited to deploy

--- pages/profile/index.jsx
+++ pages/profile/index.jsx
@@ -1,25 +1,28 @@
-import { getSession } from "next-auth/react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 //Component
 import ProfilePage from "@/components/template/profile/ProfilePage";
+import Spinner from "@/components/module/spinner/Spinner";
 
 const Profile = () => {
 
-    return <ProfilePage />
+    const { status } = useSession();
+    const [isLoged, setIsloged] = useState(false)
+    const router = useRouter();
+
+
+    useEffect(() => {
+        if (status === "unauthenticated") {
+            router.replace("/auth/signup")
+        } else if (status === "authenticated") {
+            setIsloged(true)
+        }
+    }, [status])
+
+    if (status === "loading") return <Spinner />
+    if (isLoged) return <ProfilePage />
 };
 
 export default Profile;
-
-export const getServerSideProps = async ({ req }) => {
-    const session = await getSession({ req });
-
-    if (!session) return {
-        redirect: { destination: "/auth/signup", permanent: false }
-    }
-
-    return {
-        props: {
-            session
-        }
-    }
-}
